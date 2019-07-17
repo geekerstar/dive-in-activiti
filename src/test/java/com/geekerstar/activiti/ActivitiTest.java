2214848410b82ccd50902d36b915bf3100af3797
geekerstar <247507792@qq.com>
2019-07-17 19:45:35 +0800

    一行代码创建25个表

--- src/test/java/com/geekerstar/activiti/ActivitiTest.java
+++ src/test/java/com/geekerstar/activiti/ActivitiTest.java
@@ -2,6 +2,7 @@ package com.geekerstar.activiti;
 
 import org.activiti.engine.ProcessEngine;
 import org.activiti.engine.ProcessEngineConfiguration;
+import org.activiti.engine.ProcessEngines;
 import org.junit.Test;
 
 /**
@@ -22,5 +23,16 @@ public class ActivitiTest {
         //3、输出Process是Engine对象
         System.out.println(processEngine);
 
+    }
+
+    /**
+     * 一行代码创建
+     */
+    @Test
+    public void testGenTable2(){
+        //条件1、activiti配置文件名称:activiti.cfg.xml
+        //条件2、bean的id="processEngineConfiguration"
+        ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
+        System.out.println(processEngine);
     }
 }
